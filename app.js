f4b9b6645de881ecc2f9c2a1e4b5c6cae50187a6
Kelly HANTAN <56310541+6fried@users.noreply.github.com>
2022-12-07 19:20:59 +0000

    .

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ class App {
   activateXR = async () => {
     try {
       // Initialize a WebXR session using "immersive-ar".
-      this.xrSession = await navigator.xr.requestSessino("immersive-ar");
+      this.xrSession = await navigator.xr.requestSession("immersive-ar");
 
       // Create the canvas that will contain our camera's background and our virtual scene.
       this.createXRCanvas();
