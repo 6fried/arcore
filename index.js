80362129f1f0319dfe5e3f8d1c21d9ab9a5bb1cc
6fried <kellyhantan@gmail.com>
2022-12-26 19:05:42 +0100

    test

--- index.js
+++ index.js
@@ -1,115 +1,128 @@
-
-(async function() {
-    const isArSupported = navigator.xr && navigator.xr.isSessionSupported && await navigator.xr.isSessionSupported("immersive-ar");
-    if(isArSupported) {
-        document.getElementById("enter-ar").addEventListener("click", window.app.activateXR)
-    } else {
-        onNoXRDevice();
-    }
-})();
-
-
 /**
- * Query to see if the desired XR mode is supported.
- * If support is available, advertise XR functionality to the user.
- * A user-activation event indicates that the user wishes to use XR.
- * Request an immersive session from the device
- * Use the session to run a render loop that updates sensor data, and produces graphical frames to be displayed on the XR device.
- * Continue producing frames until the user indicates that they wish to exit XR mode.
- * End the XR session. 
- **/
-
-/**
- * Web AR App container
+ * Query for WebXR support. If there's no support for the `immersive-ar` mode,
+ * show an error.
  */
-class App {
+(async function() {
+    const isArSessionSupported = navigator.xr && navigator.xr.isSessionSupported && await navigator.xr.isSessionSupported("immersive-ar");
+    if (isArSessionSupported) {
+      document.getElementById("enter-ar").addEventListener("click", window.app.activateXR)
+    } else {
+      onNoXRDevice();
+    }
+  })();
+  
+  /**
+   * Container class to manage connecting to the WebXR Device API
+   * and handle rendering on every frame.
+   */
+  class App {
     /**
-     * Run when the Start AR button is pressed
+     * Run when the Start AR button is pressed.
      */
     activateXR = async () => {
-        try {
-            this.xrSession = await navigator.xr.requestSession('immersive-ar');
-
-            this.createXRCanvas();
-
-            await this.onSessionStarted();
-        } catch(e) {
-            onNoXRDevice();
-        }
+      try {
+        // Initialize a WebXR session using "immersive-ar".
+        this.xrSession = await navigator.xr.requestSession('immersive-ar');
+  
+        // Create the canvas that will contain our camera's background and our virtual scene.
+        this.createXRCanvas();
+  
+        // With everything set up, start the app.
+        await this.onSessionStarted();
+      } catch(e) {
+        onNoXRDevice();
+      }
     }
-
+  
     /**
-     * Adds a Canvas element and initialize a WebGL context that is compatible with WebXR
+     * Add a canvas element and initialize a WebGL context that is compatible with WebXR.
      */
     createXRCanvas() {
-        this.canvas = document.createElement("canvas");
-        document.body.appendChild(this.canvas);
-        this.gl = this.canvas.getContext("webgl", {xrCompatible: true});
-
-        this.xrSession.updateRenderState({
-            baseLayer: new XRWebGLLayer(this.xrSession, this.gl)
-        });
+      this.canvas = document.createElement("canvas");
+      document.body.appendChild(this.canvas);
+      this.gl = this.canvas.getContext("webgl", {xrCompatible: true});
+  
+      this.xrSession.updateRenderState({
+        baseLayer: new XRWebGLLayer(this.xrSession, this.gl)
+      });
     }
-
+  
     /**
      * Called when the XRSession has begun. Here we set up our three.js
      * renderer and kick off the render loop.
      */
     async onSessionStarted() {
-        document.body.classList.add('ar');
-
-        this.setupThreeJS();
-
-        this.localReferenceSpace = await this.xrSession.requestReferenceSpace("local");
-
-        this.xrSession.requestAnimationFrame(this.onXRFrame);
+      // Add the `ar` class to our body, which will hide our 2D components
+      document.body.classList.add('ar');
+  
+      // To help with working with 3D on the web, we'll use three.js.
+      this.setupThreeJs();
+  
+      // Setup an XRReferenceSpace using the "local" coordinate system.
+      this.localReferenceSpace = await this.xrSession.requestReferenceSpace("local");
+  
+      // Start a rendering loop using this.onXRFrame.
+      this.xrSession.requestAnimationFrame(this.onXRFrame);
     }
-
-    /**
-     * Initialize three.js specific rendering code, including a WebGLRenderer,
-     * a demo scene, and a camera for viewing the 3D content.
-     */
-    setupThreeJS() {
-        this.renderer = new THREE.WebGLRenderer({
-            alpha: true,
-            preserveDrawingBuffer: true,
-            canvas: this.canvas,
-            context: this.gl
-        });
-        this.renderer.autoClear = false;
-
-        // this.scene = Utils.createCubeScene();
-        this.scene = Utility.createCubeScene();
-
-        this.camera = new THREE.PerspectiveCamera();
-        this.camera.matrixAutoUpdate = false;
-    }
-
+  
     /**
      * Called on the XRSession's requestAnimationFrame.
      * Called with the time and XRPresentationFrame.
      */
     onXRFrame = (time, frame) => {
-        this.xrSession.requestAnimationFrame(this.onXRFrame);
-
-        const framebuffer = this.xrSession.renderState.baseLayer.framebuffer;
-        this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, framebuffer);
-        this.renderer.SetFramebuffer(framebuffer);
-
-        const pose = frame.getViewerPose(this.localReferenceSpace);
-        if(pose) {
-            const view = pose.views[0];
-
-            const viewport = this.xrSession.renderState.baseLayer.getViewport(view);
-            this.renderer.setSize(viewport.width, viewport.height);
-
-            this.camera.matrix.fromArray(view.transform.matrix);
-            this.camera.projectionMatrix.fromArray(view.projectionMatrix);
-            this.camera.updateMatrixWorld(true);
-
-            this.renderer.render(this.scene, this.camera);
-        }
+      // Queue up the next draw request.
+      this.xrSession.requestAnimationFrame(this.onXRFrame);
+  
+      // Bind the graphincs framebuffer to the baseLayer's framebuffer.
+      const framebuffer = this.xrSession.renderState.baseLayer.framebuffer;
+      this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, framebuffer);
+      this.renderer.setFramebuffer(framebuffer);
+  
+      // Retrieve the pose of the device.
+      // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
+      const pose = frame.getViewerPose(this.localReferenceSpace);
+      if (pose) {
+        // In mobile AR, we only have one view.
+        const view = pose.views[0]; 
+  
+        const viewport = this.xrSession.renderState.baseLayer.getViewport(view);
+        this.renderer.setSize(viewport.width, viewport.height);
+  
+        // Use the view's transform matrix and projection matrix to configure the THREE.camera.
+        this.camera.matrix.fromArray(view.transform.matrix);
+        this.camera.projectionMatrix.fromArray(view.projectionMatrix);
+        this.camera.updateMatrixWorld(true);
+   
+        // Render the scene with THREE.WebGLRenderer
+        this.renderer.render(this.scene, this.camera);
+      }
     }
-};
-
-window.app = new App();
+  
+    /**
+     * Initialize three.js specific rendering code, including a WebGLRenderer,
+     * a demo scene, and a camera for viewing the 3D content.
+     */
+    setupThreeJs() { 
+      // To help with working with 3D on the web, we'll use three.js.
+      // Set up the WebGLRenderer, which handles rendering to our session's base layer.
+      this.renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        preserveDrawingBuffer: true,
+        canvas: this.canvas,
+        context: this.gl
+      });
+      this.renderer.autoClear = false;
+  
+      // Initialize our demo scene.
+      this.scene = DemoUtils.createCubeScene();
+  
+      // We'll update the camera matrices directly from API, so
+      // disable matrix auto updates so three.js doesn't attempt
+      // to handle the matrices independently.
+      this.camera = new THREE.PerspectiveCamera();
+      this.camera.matrixAutoUpdate = false;
+    }
+  };
+  
+  window.app = new App();
+  
--- shared/utils.js
+++ shared/utils.js
@@ -1,74 +1,115 @@
 window.gltfLoader = new THREE.GLTFLoader();
-
+/**
+ * The Reticle class creates an object that repeatedly calls
+ * `xrSession.requestHitTest()` to render a ring along a found
+ * horizontal surface.
+ */
 class Reticle extends THREE.Object3D {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.loader = new THREE.GLTFLoader();
-        this.loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf", (gltf) => {
-            this.add(gltf.scene);
-        })
+    this.loader = new THREE.GLTFLoader();
+    this.loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/reticle/reticle.gltf", (gltf) => {
+      this.add(gltf.scene);
+    })
 
-        this.visible = false;
-    }
+    this.visible = false;
+  }
 }
 
 window.gltfLoader.load("https://immersive-web.github.io/webxr-samples/media/gltf/sunflower/sunflower.gltf", function(gltf) {
-    const flower = gltf.scene.children.find(c => c.name === 'sunflower')
-    flower.castShadow = true;
-    window.sunflower = gltf.scene;
+  const flower = gltf.scene.children.find(c => c.name === 'sunflower')
+  flower.castShadow = true;
+  window.sunflower = gltf.scene;
 });
 
-window.Utility = {
 
-    createLitScene() {
-        const scene = new THREE.Scene();
+window.DemoUtils = {
+  /**
+   * Creates a THREE.Scene containing lights that case shadows,
+   * and a mesh that will receive shadows.
+   *
+   * @return {THREE.Scene}
+   */
+  createLitScene() {
+    const scene = new THREE.Scene();
 
-        const light = new THREE.AmbientLight(0xffffff, 1);
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
-        directionalLight.position.set(10, 15, 10);
+    // The materials will render as a black mesh
+    // without lights in our scenes. Let's add an ambient light
+    // so our material can be visible, as well as a directional light
+    // for the shadow.
+    const light = new THREE.AmbientLight(0xffffff, 1);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
+    directionalLight.position.set(10, 15, 10);
 
-        directionalLight.castShadow = true;
+    // We want this light to cast shadow.
+    directionalLight.castShadow = true;
 
-        const planeGeometry = new THREE.PlaneGeometry(2000, 2000);
-        planeGeometry.rotateX(-Math.PI / 2);
+    // Make a large plane to receive our shadows
+    const planeGeometry = new THREE.PlaneGeometry(2000, 2000);
+    // Rotate our plane to be parallel to the floor
+    planeGeometry.rotateX(-Math.PI / 2);
 
-        const shadowMesh = new THREE.Mesh(planeGeometry, new THREE.ShadowMaterial({
-            color: 0x111111,
-            opacity: 0.2,
-        }));
+    // Create a mesh with a shadow material, resulting in a mesh
+    // that only renders shadows once we flip the `receiveShadow` property.
+    const shadowMesh = new THREE.Mesh(planeGeometry, new THREE.ShadowMaterial({
+      color: 0x111111,
+      opacity: 0.2,
+    }));
 
-        shadowMesh.name = 'shadowMesh';
-        shadowMesh.receiveShadow = true;
-        shadowMesh.position.y = 10000;
+    // Give it a name so we can reference it later, and set `receiveShadow`
+    // to true so that it can render our model's shadow.
+    shadowMesh.name = 'shadowMesh';
+    shadowMesh.receiveShadow = true;
+    shadowMesh.position.y = 10000;
 
-        scene.add(shadowMesh);
-        scene.add(light);
-        scene.add(directionalLight);
+    // Add lights and shadow material to scene.
+    scene.add(shadowMesh);
+    scene.add(light);
+    scene.add(directionalLight);
 
-        return scene;
-    },
+    return scene;
+  },
 
-    createCubeScene() {
-        const scene = new THREE.Scene();
+  /**
+   * Creates a THREE.Scene containing cubes all over the scene.
+   *
+   * @return {THREE.Scene}
+   */
+  createCubeScene() {
+    const scene = new THREE.Scene();
 
-        const materials = [
-            new THREE.MeshBasicMaterial({ color: 0xff0000 }),
-            new THREE.MeshBasicMaterial({ color: 0x0000ff }),
-            new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
-            new THREE.MeshBasicMaterial({ color: 0xff00ff }),
-            new THREE.MeshBasicMaterial({ color: 0x00ffff }),
-            new THREE.MeshBasicMaterial({ color: 0xffff00 })
-        ];
+    const materials = [
+      new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+      new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+      new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+      new THREE.MeshBasicMaterial({ color: 0xff00ff }),
+      new THREE.MeshBasicMaterial({ color: 0x00ffff }),
+      new THREE.MeshBasicMaterial({ color: 0xffff00 })
+    ];
 
-        const box = new THREE.Mesh(new THREE.BoxBufferGeometry(0.2, 0.2, 0.2), materials);
-        box.position.set(1, 1, 1);
-        scene.add(box);
+    // const ROW_COUNT = 4;
+    // const SPREAD = 1;
+    // const HALF = ROW_COUNT / 2;
+    // for (let i = 0; i < ROW_COUNT; i++) {
+    //   for (let j = 0; j < ROW_COUNT; j++) {
+    //     for (let k = 0; k < ROW_COUNT; k++) {
+          const box = new THREE.Mesh(new THREE.BoxBufferGeometry(0.2, 0.2, 0.2), materials);
+          box.position.set(1, 1, 1);
+          // box.position.multiplyScalar(1); 
+          scene.add(box);
+    //     }
+    //   }
+    // }
 
-        return scene;
-    },
+    return scene;
+  },
 };
 
+/**
+ * Toggle on a class on the page to disable the "Enter AR"
+ * button and display the unsupported browser message.
+ */
 function onNoXRDevice() {
-    document.body.classList.add('unsupported');
+  document.body.classList.add('unsupported');
 }
